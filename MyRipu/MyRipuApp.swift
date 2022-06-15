07fc6976210e594819746d0cc92fff05c974631c
Apprenant 16 <apprenant16@MacBook-Pro-3.local>
2022-06-15 05:40:06 -0700

    Myfirst commit

---
+++ MyRipu/MyRipuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRipuApp.swift
+//  MyRipu
+//
+//  Created by Apprenant 16 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyRipuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
